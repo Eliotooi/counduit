918723532647c9b04b2ef554bd6fc2a4f6316ac0
Eliotool <d.dovgiy420@gmail.com>
2023-02-14 20:53:01 +0200

    Add banner

---
+++ src/common/components/banner/banner.component.tsx
@@ -0,0 +1,19 @@
+import {FC} from 'react'
+import { Container } from '../container/container.components'
+
+interface BannerProps {}
+
+export const Banner: FC<BannerProps> = ()=>{
+  return(
+    <div className='bg-conduit-green shadow-banner text-white p-8 mb-8'>
+      <Container>
+        <h1 className='font-titillium drop-shadow-logo text-center text-logo pb-2'>
+          Conduit
+        </h1>
+        <p className='text-center text-2xl font-light'>
+          A place to share your knowledge
+        </p>
+      </Container>
+    </div>
+  )
+}
